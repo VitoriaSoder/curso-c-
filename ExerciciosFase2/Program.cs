9655a7c075daf68658e5de5a24d324ee843dcb69
Vitoriasoder <soderv32@gmail.com>
2026-05-19 12:15:11 -0300

    feat: implement delta calculation and login validation exercises and update questionnaire answers

--- ExerciciosFase2/Program.cs
+++ ExerciciosFase2/Program.cs
@@ -87,6 +87,14 @@ Console.WriteLine("Módulo: " + (numero1Double % numero2Double));
 // a b e c .
 // Valores : a = 1 , b = 12 e c = -13
 
+var a = 1;
+var b = 12;
+var c = -13;
+
+var delta = (b * b) - 4 * a * c;
+
+Console.WriteLine("Delta: " + delta);
+
 
 
 //9- Escreva um programa que receba um nome e uma senha via teclado. Nome é uma string e
@@ -95,16 +103,25 @@ Console.WriteLine("Módulo: " + (numero1Double % numero2Double));
 //inválido’: (use o operador condicional ternário)
 
 
+var nome = console.ReadLine("Escreva seu nome aqui: ");
+var senha = console.ReadLine("Escreva sua senha aqui: ");
+
+
+var login = (nome == "admin" || nome == "maria") && senha == "123" ? "Login feito com sucesso" : "Login inválido";
+
+Console.WriteLine(login);
+
+
 // 10- Indique verdadeiro(V) ou falso (F) para as seguintes declarações de variáveis considerando
 // a nomenclatura usada:
-// ( ) string? nome; é um exemplo de nullable reference type;
-// ( ) para x igual a 0 , Console.WriteLine(x++); imprime o valor 1
-// ( ) A ordem de precedência dos operadores lógicos é : ! , || e && (NOT, OR e AND)
-// ( ) para y igual a 0 , Console.WriteLine(++y); imprime o valor 1
-// ( ) (10 % 2 == 0) ? “Par” : “Impar”; Vai retornar “Impar”
-// ( ) Para x=25 e y=5 , a expressão ( y >= x ) && ( y <= x ); retorna true;
-// ( ) Os nullable reference types emitem um alerta em tempo de compilação para uso do null
-// ( ) Para x = 10 , o código Console.WriteLine(x+=x); imprime o valor 10;
-// ( ) Para x= 5 , a expressão !(9 >= x) && x <= 7 || x == 5); retorna false;
-// ( ) O operador ?. permite verificar se um objeto é null e evitar o NullReferenceException
-// ( ) Qualquer tipo de dado pode ser declarado como anulável com a ajuda do operador "?"
+// (V) string? nome; é um exemplo de nullable reference type;
+// (F) para x igual a 0 , Console.WriteLine(x++); imprime o valor 1
+// (V) A ordem de precedência dos operadores lógicos é : ! , || e && (NOT, OR e AND)
+// (V) para y igual a 0 , Console.WriteLine(++y); imprime o valor 1
+// (V) (10 % 2 == 0) ? “Par” : “Impar”; Vai retornar “Impar”
+// (F) Para x=25 e y=5 , a expressão ( y >= x ) && ( y <= x ); retorna true;
+// (V) Os nullable reference types emitem um alerta em tempo de compilação para uso do null
+// (F) Para x = 10 , o código Console.WriteLine(x+=x); imprime o valor 10;
+// (V) Para x= 5 , a expressão !(9 >= x) && x <= 7 || x == 5); retorna false;
+// (V) O operador ?. permite verificar se um objeto é null e evitar o NullReferenceException
+// (V) Qualquer tipo de dado pode ser declarado como anulável com a ajuda do operador "?"
