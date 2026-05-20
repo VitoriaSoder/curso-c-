e016f80c11d24cc6eea87e559cd862d331667551
Vitoriasoder <soderv32@gmail.com>
2026-05-20 12:09:07 -0300

    feat: implement exercises 11 through 15 in Program.cs

--- ExerciciosFase2/Program.cs
+++ ExerciciosFase2/Program.cs
@@ -130,13 +130,58 @@ Console.WriteLine(login);
 //11- Escreva um programa que recebe via teclado dois números inteiros x e y e imprima no
 //console se x é par ou não e se y é par ou não. Use o operador condicional ternário (? :)
 
+var y = Convert.ToInt32(Console.ReadLine("Digite um número"));
+var x = Convert.ToInt32(Console.ReadLine("Digite um número"));
 
+var parOrImparX = x % 2 == 0 ? "Par" : "Impar";
+var parOrImparY = y % 2 == 0 ? "Par" : "Impar";
+
+Console.WriteLine("X é: " + parOrImparX);
+Console.WriteLine("Y é: " + parOrImparY);
 
 // 12- Crie um programa que receba um numero inteiro x via teclado e calcule e imprima no
 // console o resultado das seguintes operações : (x^2 -> x ao quadrado) (pi = 3.1415)
-// -6 + x * 5
-// (13-2) * x
-// (x + -2) * (20 / x)
-// (12 + x) / (x - 4)
-// 3 x^2 + x + 10
-// pi * x^2
+
+int x = Convert.ToInt32(Console.ReadLine("Digite um número"));
+
+Console.WriteLine("-6 + x * 5: " + (-6 + x * 5));
+Console.WriteLine("(13-2) * x: " + ((13-2) * x));
+Console.WriteLine("(x + -2) * (20 / x): " + ((x + -2) * (20 / x)));
+Console.WriteLine("(12 + x) / (x - 4): " + ((12 + x) / (x - 4)));
+Console.WriteLine("3 x^2 + x + 10: " + (3 * x * x + x + 10));
+Console.WriteLine("pi * x^2: " + (3.1415 * x * x));
+
+
+// 13- Considere o seguinte trecho de código:
+int y = 5 ;
+y = (y++)+y+(++y); ou y = y++ + y + ++y;
+Console.WriteLine(y);
+
+
+// 14- Escreva um programa que solicite a temperatura em graus Celsius e converta para
+// Kelvin e Farhenheit usando as fórmulas a seguir:
+// - Converter para Kelvin => K = C + 273 ;
+// - Converter para Farhenheit => F = (C * 9) / 5 + 32 ;
+
+var celsius = Convert.ToInt32(Console.ReadLine("Digite a temperatura em graus Celsius"));
+
+Console.WriteLine("Temperatura em Kelvin: " + (celsius + 273));
+Console.WriteLine("Temperatura em Farhenheit: " + ((celsius * 9) / 5 + 32));
+
+
+
+// 15 - Escolha a opção que representa a exibição do resultado para o código usando os
+// operadores de decremento e incremento (pré e pós) :
+// var numero = 5;
+// Console.WriteLine(numero++);
+// numero = 1;
+// Console.WriteLine(++numero);
+// numero = 2;
+// Console.WriteLine(numero--);
+// numero = 3;
+// Console.WriteLine(--numero);
+// Console.ReadKey();
+
+
+//res: O resultado para essa operação é a sequencia (5, 2, 2, 2), o ++ na frente é representado como antes de imprimir o valor ele soma 1 e o -- na frente é representado como antes de imprimir o valor ele subtraiu 1
+
